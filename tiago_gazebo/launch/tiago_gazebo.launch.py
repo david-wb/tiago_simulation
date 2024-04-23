d3f6cff2945c7cc0e931a33eeb0c7aefe39260fc
Oscar <oscarmartinez@pal-robotics.com>
2024-04-23 09:50:21 +0200

    Suggested changes

--- tiago_gazebo/launch/tiago_gazebo.launch.py
+++ tiago_gazebo/launch/tiago_gazebo.launch.py
@@ -15,47 +15,33 @@
 import os
 from os import environ, pathsep
 from ament_index_python.packages import get_package_prefix
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
-
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, SetLaunchConfiguration
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
-
 from launch_pal.include_utils import include_scoped_launch_py_description
-
-from launch_ros.actions import Node
 from launch_pal.arg_utils import LaunchArgumentsBase
-from launch_pal.robot_arguments import TiagoArgs
 from dataclasses import dataclass
+from launch_pal.arg_utils import CommonArgs
+from launch_ros.actions import Node
+from launch_pal.robot_arguments import TiagoArgs
 
 
 @dataclass(frozen=True)
 class LaunchArguments(LaunchArgumentsBase):
 
     base_type: DeclareLaunchArgument = TiagoArgs.base_type
+    arm_type: DeclareLaunchArgument = TiagoArgs.arm_type
+    end_effector: DeclareLaunchArgument = TiagoArgs.end_effector
+    ft_sensor: DeclareLaunchArgument = TiagoArgs.ft_sensor
+    wrist_model: DeclareLaunchArgument = TiagoArgs.wrist_model
     camera_model: DeclareLaunchArgument = TiagoArgs.camera_model
     laser_model: DeclareLaunchArgument = TiagoArgs.laser_model
-    has_screen: DeclareLaunchArgument = TiagoArgs.has_screen
-    wrist_model: DeclareLaunchArgument = TiagoArgs.wrist_model
-    ft_sensor: DeclareLaunchArgument = TiagoArgs.ft_sensor
-    end_effector: DeclareLaunchArgument = TiagoArgs.end_effector
-    arm_type: DeclareLaunchArgument = TiagoArgs.arm_type
 
-    navigation: DeclareLaunchArgument = DeclareLaunchArgument(
-        name='navigation',
-        default_value='False',
-        description='Specify if launching Navigation2')
-
-    moveit: DeclareLaunchArgument = DeclareLaunchArgument(
-        name='moveit',
-        default_value='True',
-        description='Specify if launching MoveIt 2')
-
-    world_name: DeclareLaunchArgument = DeclareLaunchArgument(
-        name='world_name',
-        default_value='pal_office',
-        description="Specify world name, we'll convert to full path")
+    navigation: DeclareLaunchArgument = CommonArgs.navigation
+    moveit: DeclareLaunchArgument = CommonArgs.moveit
+    world_name: DeclareLaunchArgument = CommonArgs.world_name
+    namespace: DeclareLaunchArgument = CommonArgs.namespace
 
     public_sim: DeclareLaunchArgument = DeclareLaunchArgument(
         name='public_sim',
@@ -76,12 +62,17 @@ def generate_launch_description():
     return ld
 
 
-def declare_actions(launch_description: LaunchDescription,
-                    launch_args: LaunchArguments):
+def declare_actions(
+    launch_description: LaunchDescription, launch_args: LaunchArguments
+):
+    # Set use_sim_time to True
+    set_sim_time = SetLaunchConfiguration("use_sim_time", "True")
+    launch_description.add_action(set_sim_time)
+
     robot_name = 'tiago'
     packages = ['tiago_description', 'pmb2_description',
                 'pal_hey5_description', 'pal_gripper_description',
-                'pal_robotiq_description']
+                'pal_robotiq_description', 'omni_base_description']
 
     model_path = get_model_paths(packages)
 
@@ -102,7 +93,13 @@ def declare_actions(launch_description: LaunchDescription,
 
     navigation = include_scoped_launch_py_description(
         pkg_name='tiago_2dnav',
-        paths=['launch', 'tiago_nav_bringup.launch.py'],
+        paths=['launch', 'tiago_sim_nav_bringup.launch.py'],
+        launch_arguments={
+            "robot_name":  robot_name,
+            "is_public_sim": launch_args.public_sim,
+            "laser":  launch_args.laser_model,
+            "base_type": launch_args.base_type
+        },
         condition=IfCondition(LaunchConfiguration('navigation')))
 
     launch_description.add_action(navigation)
@@ -119,7 +116,7 @@ def declare_actions(launch_description: LaunchDescription,
 
     robot_spawn = include_scoped_launch_py_description(
         pkg_name='tiago_gazebo',
-        paths=['launch', 'tiago_spawn.launch.py'],
+        paths=['launch', 'robot_spawn.launch.py'],
         launch_arguments={
             'robot_name': robot_name,
             'base_type': launch_args.base_type}
@@ -130,14 +127,15 @@ def declare_actions(launch_description: LaunchDescription,
     tiago_bringup = include_scoped_launch_py_description(
         pkg_name='tiago_bringup', paths=['launch', 'tiago_bringup.launch.py'],
         launch_arguments={
-            'use_sim_time': 'True',
+            "use_sim_time": LaunchConfiguration("use_sim_time"),
             "laser_model": launch_args.laser_model,
             "camera_model": launch_args.camera_model,
             "base_type": launch_args.base_type,
             "wrist_model": launch_args.wrist_model,
             "ft_sensor": launch_args.ft_sensor,
             "end_effector": launch_args.end_effector,
-            "has_screen": launch_args.has_screen}
+            "has_screen": launch_args.has_screen
+        }
     )
 
     launch_description.add_action(tiago_bringup)
@@ -153,13 +151,13 @@ def declare_actions(launch_description: LaunchDescription,
 
 
 def get_model_paths(packages_names):
-    model_paths = ''
+    model_paths = ""
     for package_name in packages_names:
-        if model_paths != '':
+        if model_paths != "":
             model_paths += pathsep
 
         package_path = get_package_prefix(package_name)
-        model_path = os.path.join(package_path, 'share')
+        model_path = os.path.join(package_path, "share")
 
         model_paths += model_path
 
@@ -167,18 +165,3 @@ def get_model_paths(packages_names):
         model_paths += pathsep + environ['GAZEBO_MODEL_PATH']
 
     return model_paths
-
-
-def get_resource_paths(packages_names):
-    resource_paths = ''
-    for package_name in packages_names:
-        if resource_paths != '':
-            resource_paths += pathsep
-
-        package_path = get_package_prefix(package_name)
-        resource_paths += package_path
-
-    if 'GAZEBO_RESOURCE_PATH' in environ:
-        resource_paths += pathsep + environ['GAZEBO_RESOURCE_PATH']
-
-    return resource_paths
